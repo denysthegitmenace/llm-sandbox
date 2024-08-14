7cebe62742f0b3469160523aab50bf50eb9d8f0b
denysthegitmenace <denystetsenko@gmail.com>
2024-08-14 09:57:51 +0200

    first haystack pipeline

---
+++ inputs.py
@@ -0,0 +1,127 @@
+MATRIX = """
+<matrix>
+    <category name="Vertrauen bilden">
+        <skill name="Kunden verstehen">
+            <rating>3.5</rating>
+        </skill>
+        <skill name="Wertschätzend zusammenarbeiten">
+            <rating>2</rating>
+        </skill>
+        <skill name="Andere gewinnen">
+            <rating>2</rating>
+        </skill>
+    </category>
+    <category name="Leistungskultur fordern">
+        <skill name="Leistung erbringen">
+            <rating>3</rating>
+        </skill>
+        <skill name="Auf Effizienz fokussieren">
+            <rating>3.5</rating>
+        </skill>
+        <skill name="Unternehmenswert schaffen">
+            <rating>3</rating>
+        </skill>
+    </category>
+    <category name="Mutig handeln">
+        <skill name="Intelligente Lösungen entwickeln">
+            <rating>2.5</rating>
+        </skill>
+        <skill name="Neue Wege gehen">
+            <rating>2.5</rating>
+        </skill>
+        <skill name="Veränderung leben">
+            <rating>2.5</rating>
+        </skill>
+    </category>
+    <category name="In Menschen investieren">
+        <skill name="Zukunftsorientierung führen">
+            <rating>2</rating>
+        </skill>
+        <skill name="Orientierung geben">
+            <rating>2</rating>
+        </skill>
+        <skill name="Vertrauen leben">
+            <rating>2.5</rating>
+        </skill>
+    </category>
+</matrix>
+"""
+
+NOTES_PER_EXERCISE =  {
+"Präsentation und Interview 1":
+"""<observation_notes>
+    <strengths>
+        <strength>Tritt insgesamt in Gesprächen sowie Interviewsituationen kommunikationsoffen und zugleich professionell auf</strength>
+        <strength>Beweist im Arbeitsstil eine hohe Strukturorientierung, logisches Vorgehen sowie eine fokussierte Ergebnisgenerierung</strength>
+        <strength>Behält das Ziel der Aufgabe im Fokus und erfüllt Aufgaben anforderungsgerecht</strength>
+        <strength>Geht mit Offenheit an zwischenmenschliche Beziehungen heran und besitzt daher ein großes Netzwerk bei Kunde X; greift auf dieses Netzwerk zurück, um Herausforderungen im Schulterschluss zu bewältigen, wie bspw. beim Onboarding von neuen Mitarbeitenden</strength>
+        <strength>Zeigt in der Erfüllung von Arbeitszielen eine hohe Effizienzorientierung und vermittelt in seiner Rolle die Kompetenz, als verlässlicher Partner aufzutreten sowie Umsetzungen von Projekten erfolgreich zu steuern</strength>
+        <strength>Beweist Wirksamkeit bei vergangenen Erfolgen und verhält sich weiterhin leistungsmotiviert, um diese Erfolge aufrechtzuerhalten und weiter zu gestalten</strength>
+        <strength>Vermittelt einen gesamtheitlichen Blick über Prozesse und Strukturen in seinem Verantwortungsbereich; verfolgt das intrinsische Ziel, diese Prozesse hinsichtlich der Effizienz am Leben zu erhalten sowie den Puls des Zusammenarbeitsgeflechts zu erspüren</strength>
+        <strength>Besitzt ein positives und optimistisches Menschenbild; überträgt dieses Bild auf das Fundament seines Führungsansatzes; hegt viel Vertrauen in andere Menschen und überlässt Freiräume zur Selbstgestaltung</strength>
+        <strength>Geht erfahrungs- und lernoffen an Situationen heran und generiert dadurch Zugewinn für sein eigenes Wissen durch das Know-how von anderen (bspw. in der Zusammenarbeit mit Beratenden oder durch die Generierung von Wissen durch Fachexperten)</strength>
+        <strength>Hinterlässt mit seiner Arbeit eine aussagekräftige Note sowie Erinnerungswert auch auf der Ebene des Vorstands und kennt die entsprechenden Andockungspunkte, um in den verschiedenen Kreisen eine konsistente Geschichte zu erzählen, andere zu überzeugen und mitzunehmen</strength>
+        <strength>Gewinnt das Vertrauen von anderen durch eine fokussierte Darstellung von Ergebnissen sowie ein zielorientiertes Handeln bei erfolgreicher Vermeidung, sich selbst in der Komplexität zu verlieren</strength>
+    </strengths>
+    <development_opportunities>
+        <opportunity>Spricht eher nüchtern und klar; könnte mit diesem Sprachstil bei einigen Persönlichkeiten die Herausforderung besitzen, mit der nötigen Emotionalität zu inspirieren</opportunity>
+        <opportunity>Dürfte sich noch mehr mit anderen Leitungsfunktionen aus dem M&A-Bereich austauschen, um Impulse für die eigene Ausgestaltung des Bereichs zu erhalten; zeigt aktuell eher die Tendenz, als aufgabenfokussierter Umsetzer aufzutreten; er dürfte daher seine Fähigkeiten in der Gestaltung von Visionen steigern</opportunity>
+        <opportunity>Könnte das Thema Führung noch differenzierter betrachten und sein Selbstverständnis intensiver reflektieren, damit er das Bild der erweiterten Ressourcenverfügbarkeit als Treiber in die nächste Führungsrolle durch weitere Facetten bekräftigt (vorrangiger aktueller Führungsantrieb „Ich kann über weitere Ressourcen verfügen“)</opportunity>
+    </development_opportunities>
+</observation_notes>""",
+"Fallstudie":
+"""<observation_notes>
+    <strengths>
+        <strength>Tritt in der Präsentationssituation selbstsicher und souverän auf</strength>
+        <strength>Handelt aufgabenorientiert und fokussiert sich auf das zu erreichende Ziel</strength>
+        <strength>Arbeitet sich strukturiert durch die Aufgabenstellung und vermittelt eine logische Abfolge in seiner Präsentation, sodass es für andere einfach bleibt, zu folgen</strength>
+        <strength>Fokussiert sich auf die Erfolgskommunikation und Prozessoptimierung, um den gestellten Business Case zu lösen</strength>
+        <strength>Zeigt grundsätzlich eine hohe Offenheit, mit verschiedenen Stakeholdern zu kommunizieren, um die Idee des Veränderungsvorhabens zunächst mit verschiedenen Parteien zu besprechen und dadurch Risiken abzubauen</strength>
+        <strength>Bleibt in der Präsentation interaktiv und beweist eine Moderationskompetenz, Fragen flexibel aufzunehmen sowie entsprechend zu bedienen</strength>
+        <strength>Vermittelt eine hohe Kundenorientierung und bemisst den Erfolg seiner Vorhaben an den Kundenmehrwert sowie an der Kundenbindung</strength>
+        <strength>Baut auf seinen vergangenen Erfahrungen auf und generiert daraus Ideen zur Lösung der Herausforderungen (bspw. Implementierung eines kleineren Piloten in einem risikoarmen Bereich)</strength>
+    </strengths>
+    <development_opportunities>
+        <opportunity>Kommt schnell zum Punkt und verpasst es dadurch, zunächst einen Rahmen zu setzen und das Publikum angemessen für das Thema aufzugleisen; dürfte hier noch mehr Zeit investieren, um zunächst Rapport sowie Relevanz des Themas bei den Zuhörenden aufzubauen, bevor er in die Thematik einsteigt</opportunity>
+        <opportunity>Könnte sein Vorgehen noch mehr systematisieren und Methoden sowie Modelle anwenden (bspw. Change-Management), um seine Arbeitsschritte in der Umsetzungsphase anhand eines Modells einzubetten und ein zu kleinteiliges Vorgehen zu vermeiden</opportunity>
+        <opportunity>Könnte noch mehr innovative und mutige Ideen zeigen, um neue Wege zu gehen und die alten sowie bekannten Strukturen zu verlassen</opportunity>
+    </development_opportunities>
+</observation_notes>
+""",
+"Mitarbeitergespräch 1 und Interview 2":
+"""<observation_notes>
+    <strengths>
+        <strength>Vermittelt insgesamt innerhalb des Gesprächs eine offene Haltung gegenüber seinen Mitarbeitenden und führt das Gespräch vorrangig mit Fragen</strength>
+        <strength>Unterteilt Lösungen in kurzfristig und langfristig, sodass er den akuten Problemen begegnen kann und gleichzeitig eine langfristige Einflussnahme auf die Herausforderungen trotzdem nicht aus dem Blickfeld verliert</strength>
+        <strength>Vermittelt eine sich selbst zurücknehmende Führungshaltung, indem er den Eigenanspruch hegt, sich mit den Mitarbeitenden weiterzuentwickeln und noch nicht ausgelernt zu haben</strength>
+        <strength>Überlässt der Mitarbeiterin Freiraum zur Selbstgestaltung und demonstriert den Anspruch, Verantwortung abzugeben sowie andere konkret einzubinden</strength>
+        <strength>Vertraut dabei auf die Kompetenzen der Mitarbeiterin und gibt ihr den nötigen Freiraum (vermeidet Micromanaging)</strength>
+        <strength>Führt das Gespräch neben der erfahrungsoffenen Haltung auch zugleich strukturiert; fasst das Gespräch zum Ende zusammen</strength>
+        <strength>Überlegt erste Entwicklungsmaßnahmen im klassischen Ansatz (bspw. interner Trainingskatalog), um der Mitarbeiterin Maßnahmen anzubieten</strength>
+        <strength>Erkennt in der Nachreflexion mögliche Verbesserungspunkte für ein zukünftiges Führungsverhalten hinsichtlich dieser Mitarbeiterin</strength>
+    </strengths>
+    <development_opportunities>
+        <opportunity>Hält sich mit seinen Führungsimpulsen eher zurück und könnte mehr Ideen reingeben, woraus die Mitarbeiterin ihre eigenen Lösungen ableiten kann; hofft stark darauf, dass die Mitarbeiterin mit eigenen Lösungen kommt und läuft dann Gefahr, sich mit undifferenzierten Lösungen zufriedenzugeben</opportunity>
+        <opportunity>Könnte seine Führungsmethoden in Gesprächssituationen anreichern, sodass er weitere Techniken zeigen dürfte (bspw. Perspektivenwechsel, entwicklungsorientierte Impulse etc.)</opportunity>
+        <opportunity>Könnte das Thema der Konfliktlösung eingehender mit seiner Mitarbeiterin reflektieren sowie den zwischenmenschlichen Ursprung des Konflikts mit ihr aufarbeiten; zeigt die Tendenz, psychologische Themen im eigenen Führungsauftrag eher auszulagern; er dürfte hier noch mehr in die Verantwortung gehen, in solchen Situationen als Führungskraft unterstützend reinzugehen, bevor er weitere Parteien mit ins Boot holt</opportunity>
+    </development_opportunities>
+</observation_notes>
+""", 
+"Mitarbeitergespräch 2 und Interview 3":
+"""<observation_notes>
+    <strengths>
+        <strength>Vermittelt eine erfahrungsoffene Haltung und exploriert gemeinsam mit seinem Gegenüber die Problemstellungen in der Zusammenarbeit</strength>
+        <strength>Transportiert eine hohe Lernoffenheit sowie die Flexibilität, zuvor genannte Entwicklungsfelder aus den vorangegangenen Übungen direkt zu implementieren; zeigt dadurch die Fähigkeit, Feedback anzunehmen sowie in Verhaltensänderungen direkt umzusetzen</strength>
+        <strength>Bietet der Mitarbeiterin Unterstützung bei ihren Problemstellungen an und übernimmt dadurch Führungsverantwortung bei der Lösung des Problems</strength>
+        <strength>Reflektiert sein eigenes Führungsverhalten dahin gehend, dass er eine offene Feedbackkultur lebt und dieses Verhalten auch bei seinen Mitarbeitenden fördern möchte</strength>
+    </strengths>
+    <development_opportunities>
+        <opportunity>Fühlt sich sichtlich unwohler bei der Umsetzung von steuernden Mitarbeitergesprächen, bei denen er aus seiner Perspektive mehr steuern und klare Ansagen geben muss</opportunity>
+        <opportunity>Zeigt daher die Tendenz, zu übersteuern und punktuell an einigen Stellen des Gesprächs zu harsch und direkt in der Aussage aufzutreten; dürfte daher noch mehr Balance zwischen einer klaren Schilderung der Erwartungshaltung sowie dem Angebot der Hilfestellung als Führungskraft herstellen</opportunity>
+        <opportunity>Zeigt möglicherweise in der Führung die Tendenz, in fachlichen Inhalten abzutauchen, sodass er Gefahr läuft, nicht ausreichend Zeit für andere Themen freizuräumen</opportunity>
+        <opportunity>Besitzt für diese Herausforderung noch keine zielführenden Ansätze, um diesem Problem erfolgreich zu begegnen</opportunity>
+        <opportunity>Könnte in der Führung das psychologische Verständnis von Menschen als Kernherausforderungen noch eingehender reflektieren, um die zukünftigen Themen noch besser antizipieren zu können</opportunity>
+        <opportunity>Könnte im Rollenwechsel von einem Experten in seiner aktuellen Position in eine zukünftige Führungsrolle noch intensiver reflektieren sowie die Verhaltensänderungen, die daraus resultieren, für sich selbst noch mehr ableiten</opportunity>
+    </development_opportunities>
+</observation_notes>
+"""}
